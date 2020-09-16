9492007a50d23ce9dec3878faa5900c1270dfa99
sharmh34 <71384247+sharmh34@users.noreply.github.com>
2020-09-16 18:28:39 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,6 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, This is my Test Build for Jenkins"); 
-    }
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, This is my Test Build for Jenkins");
+  }
 }
